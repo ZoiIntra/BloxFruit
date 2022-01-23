33a9aac5d7c8b410ee85823c0f219217c64cd611
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2022-01-23 15:33:03 +0700

    Delete BountyFarm.lua

--- BountyFarm.lua
+++
@@ -1,5 +0,0 @@
-getgenv().SpawnDelay = 5
-getgenv().Hight = 30
-getgenv().Behind = 10
-getgenv().Time_to_Hop = 150
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Strikehubv2z/strikehubv2/main/autobounty.lua"))()
